1bde3dbe707a5834c602b7922be8e655f9bd299b
sarthak srivastava <sarthaksrivastava39@gmail.com>
2021-11-17 18:42:06 +0530

    First commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast = (latitiude,longtitude,callback) =>{
+    const url ='http://api.weatherstack.com/current?access_key=924f668f92cea584cf06e1fae61271fb&query='+encodeURIComponent(latitiude)+','+encodeURIComponent(longtitude) +'&units=m';
+    request({ url, json:true}, (error,{ body }= {} )=>{
+        if(error){
+            callback('Unable to connect to server',undefined)
+        }else if  (body.error){
+            callback('unable to find location',undefined)
+        }
+        else{
+            callback(undefined,(
+                body.current.weather_descriptions[0] +' throughout the day. It is currently '+
+                body.current.temperature + ' degree out there. It feels like '+
+                body.current.feelslike+' degree'    
+            ))
+        }
+    }) 
+}
+module.exports = forecast
